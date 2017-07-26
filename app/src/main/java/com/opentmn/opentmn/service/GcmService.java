9b762f16a1e5fdb7729a2abb1b23518e4315deb3
EgorovA <mrlogos91@gmail.com>
2017-07-26 12:48:36 +0500

    Изменение иконки пуш уведомления

--- app/src/main/java/com/opentmn/opentmn/service/GcmService.java
+++ app/src/main/java/com/opentmn/opentmn/service/GcmService.java
@@ -4,6 +4,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
@@ -77,12 +78,15 @@ public class GcmService extends FirebaseMessagingService {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
+        Context context = getApplicationContext();
+        Resources res = context.getResources();
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setContentTitle(getString(R.string.app_name))
                 .setContentText(message)
-                .setSmallIcon(R.mipmap.app_icon)
+                .setLargeIcon(BitmapFactory.decodeResource(res, R.mipmap.app_icon))
+                .setSmallIcon(R.drawable.app_status_icon)
                 .setPriority(NotificationCompat.PRIORITY_MAX)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
